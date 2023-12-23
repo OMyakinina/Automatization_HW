93b446b59f39557e55040d355b594dc0c1c1e064
OMyakinina <123108827+OMyakinina@users.noreply.github.com>
2023-12-23 15:35:04 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,17 +1,10 @@
 import pytest
-import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.core import manager
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-
 from check_post import get_login
 
-with open('testdata.yaml') as f:
-    testdata = yaml.safe_load(f)
-    browser = testdata['browser']
-# browser = "firefox"
 
 @pytest.fixture()
 def token():
@@ -19,13 +12,13 @@ def token():
 
 @pytest.fixture(scope="session")
 def browser():
-    # if browser == 'firefox':
-    service = Service(executable_path=GeckoDriverManager().install())
-    options = webdriver.FirefoxOptions()
-    driver = webdriver.Firefox(service=service, options=options)
-    # else:
-    #     service = Service(executable_path=ChromeDriverManager().install())
-    #     options = webdriver.ChromeOptions()
-    #     driver = webdriver.Chrome(service=service, options=options)
+    if browser == 'firefox':
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
